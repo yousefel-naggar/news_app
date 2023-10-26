91dd0f0b1b6922d834f696cff2e54a38b669dc9f
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 22:05:25 +0300

    view tab widget in tab bar and send api source name to it

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,14 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:news/models/SourcesResponse.dart';
+import 'package:news/screens/%20widget/tab_widget.dart';
 
-class TabsScreen extends StatelessWidget {
-  const TabsScreen({super.key});
+class TabsScreen extends StatefulWidget {
+  List<Sources> source;
+
+  TabsScreen(this.source, {super.key});
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  int _currentIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-
+          DefaultTabController(
+              length: widget.source.length,
+              child: TabBar(
+                isScrollable: true,
+                onTap: (value) {
+                  _currentIndex = value;
+                  setState(() {});
+                },
+                indicatorColor: Colors.transparent,
+                tabs: widget.source
+                    .map(
+                      (e) => TabWidget(e.name ?? "",
+                          widget.source.indexOf(e) == _currentIndex),
+                    )
+                    .toList(),
+              ))
         ],
       ),
     );
