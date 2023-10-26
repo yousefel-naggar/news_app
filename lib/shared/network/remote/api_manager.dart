177027e358390d30d78bb1290b0851139711876b
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 19:38:10 +0300

    make the func static

--- lib/shared/network/remote/api_manager.dart
+++ lib/shared/network/remote/api_manager.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart';
 import 'package:news/models/SourcesResponse.dart';
 
 class ApiManager {
-  Future<SourcesResponse> getSources() async{
+  static Future<SourcesResponse> getSources() async{
     Uri url = Uri.https("newsapi.org", "/v2/top-headlines/sources",
         {"apiKey": "a423944efdbe47e1aba0e510decf7c95"});
     Response response =await http.get(url);
