92c6c8ebacbd6ace5fbf0de549e928680a75ee80
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 22:23:56 +0300

    add function get news

--- lib/shared/network/remote/api_manager.dart
+++ lib/shared/network/remote/api_manager.dart
@@ -2,15 +2,26 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:http/http.dart';
+import 'package:news/models/NewsResponse.dart';
 import 'package:news/models/SourcesResponse.dart';
 
 class ApiManager {
-  static Future<SourcesResponse> getSources() async{
+  static Future<SourcesResponse> getSources() async {
     Uri url = Uri.https("newsapi.org", "/v2/top-headlines/sources",
         {"apiKey": "a423944efdbe47e1aba0e510decf7c95"});
-    Response response =await http.get(url);
+    Response response = await http.get(url);
     var jsonData = jsonDecode(response.body);
     SourcesResponse data = SourcesResponse.fromJson(jsonData);
     return data;
+  }
+
+  static Future<NewsResponse> getNews(String sourceId) async {
+    Uri url = Uri.parse(
+        "https://newsapi.org/v2/everything?apiKey=a423944efdbe47e1aba0e510decf7c95&sources=$sourceId");
+
+    http.Response response = await http.get(url);
+
+    var jsonData = jsonDecode(response.body);
+    return NewsResponse.fromJson(jsonData);
   }
 }
