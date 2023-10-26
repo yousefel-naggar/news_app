5840d4821f8230b447d1bf9f4def4cc531dad92f
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 23:16:34 +0300

    create a card contain the image of new and the source of new and title and the time that new published at

---
+++ lib/screens/ widget/news_widget.dart
@@ -0,0 +1,62 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../models/NewsResponse.dart';
+
+class NewsWidget extends StatelessWidget {
+  Articles article;
+
+  NewsWidget(this.article, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 8,
+      child: Container(
+        //color: Colors.red,
+        padding: REdgeInsets.symmetric(horizontal: 8, vertical: 7),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(15), topRight: Radius.circular(15)),
+              child: CachedNetworkImage(
+                imageUrl: article.urlToImage ?? "",
+                height: 200.h,
+                fit: BoxFit.fill,
+                placeholder: (context, url) =>
+                    Center(child: CircularProgressIndicator()),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+            SizedBox(
+              height: 6.h,
+            ),
+            Text("${article.source?.name} *" ?? "",
+                style: TextStyle(color: Colors.grey, fontSize: 13.sp)),
+            SizedBox(
+              height: 6.h,
+            ),
+            Text(
+              article.title ?? "",
+              maxLines: 2,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.sp,
+                  overflow: TextOverflow.ellipsis),
+            ),
+            SizedBox(
+              height: 4.h,
+            ),
+            Text(article.publishedAt ?? "",
+                style: TextStyle(color: Colors.grey, fontSize: 13.sp),
+                textAlign: TextAlign.end),
+          ],
+        ),
+      ),
+    );
+  }
+}
