c1a5ff90e3b99957427bbb4c19e4603cfdc57915
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 18:59:57 +0300

    create api_manager file and create function that return sources of news

---
+++ lib/shared/network/remote/api_manager.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:news/models/SourcesResponse.dart';
+
+class ApiManager {
+  Future<SourcesResponse> getSources() async{
+    Uri url = Uri.https("newsapi.org", "/v2/top-headlines/sources",
+        {"apiKey": "a423944efdbe47e1aba0e510decf7c95"});
+    Response response =await http.get(url);
+    var jsonData = jsonDecode(response.body);
+    SourcesResponse data = SourcesResponse.fromJson(jsonData);
+    return data;
+  }
+}
