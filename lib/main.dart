4b5ba7497bc227261dd1dedd58eb336aa76317f7
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 19:39:25 +0300

    make the initialRoute to home ;ayout screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news/layout/layout_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      debugShowCheckedModeBanner: false,
+      routes: {
+        HomeLayout.routeName:(context) => HomeLayout(),
+      },
+      initialRoute: HomeLayout.routeName,
     );
   }
 }
