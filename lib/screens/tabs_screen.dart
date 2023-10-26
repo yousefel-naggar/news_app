cd5569dfa6fa0b2afd064d045b323a89b814e764
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 19:41:54 +0300

    create and init tabs screen

---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class TabsScreen extends StatelessWidget {
+  const TabsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
