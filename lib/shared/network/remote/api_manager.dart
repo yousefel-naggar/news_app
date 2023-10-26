64b96cda0b6a4a9a5167eff7ccb0d930f50973ce
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-27 00:04:39 +0200

    add category parameter to get source func to choese category

--- lib/shared/network/remote/api_manager.dart
+++ lib/shared/network/remote/api_manager.dart
@@ -6,9 +6,11 @@ import 'package:news/models/NewsResponse.dart';
 import 'package:news/models/SourcesResponse.dart';
 
 class ApiManager {
-  static Future<SourcesResponse> getSources() async {
-    Uri url = Uri.https("newsapi.org", "/v2/top-headlines/sources",
-        {"apiKey": "a423944efdbe47e1aba0e510decf7c95"});
+  static Future<SourcesResponse> getSources(String category) async {
+    Uri url = Uri.https("newsapi.org", "/v2/top-headlines/sources", {
+      "apiKey": "a423944efdbe47e1aba0e510decf7c95",
+      "category": category,
+    });
     Response response = await http.get(url);
     var jsonData = jsonDecode(response.body);
     SourcesResponse data = SourcesResponse.fromJson(jsonData);
