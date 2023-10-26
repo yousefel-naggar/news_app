7a8ca6b5ae30c9d475204ffbd40bf5ad3f5b2977
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-26 22:05:45 +0300

    view tabs screen

--- lib/layout/layout_screen.dart
+++ lib/layout/layout_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news/screens/tabs_screen.dart';
 import 'package:news/shared/network/remote/api_manager.dart';
 
 class HomeLayout extends StatelessWidget {
@@ -7,18 +8,27 @@ class HomeLayout extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FutureBuilder(future: ApiManager.getSources(), builder: (context, snapshot) {
-        if(snapshot.connectionState == ConnectionState.waiting){
-          return Center(child: CircularProgressIndicator(color: Colors.green,));
-        }
-        if(snapshot.hasError){
-          return Center(child: Text("somthing wrong"),);
-        }
-        var source = snapshot.data?.sources ?? [];
-        return ListView.builder(itemBuilder: (context, index) {
-          return Text(source[index].name ?? "");
-        },itemCount: source.length,);
-      },),
+      appBar: AppBar(
+        title: Text("News app"),
+      ),
+      body: FutureBuilder(
+        future: ApiManager.getSources(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+                child: CircularProgressIndicator(
+              color: Colors.green,
+            ));
+          }
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("somthing wrong"),
+            );
+          }
+          var source = snapshot.data?.sources ?? [];
+          return TabsScreen(source);
+        },
+      ),
     );
   }
 }
