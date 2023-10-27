8f2faad992755c80194da15321a12692cfa34977
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-27 02:29:13 +0200

    create call back func to get selcted category and show this category news

--- lib/layout/layout_screen.dart
+++ lib/layout/layout_screen.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news/models/category_model.dart';
+import 'package:news/screens/category_screen.dart';
 import 'package:news/screens/tabs_screen.dart';
 import 'package:news/shared/network/remote/api_manager.dart';
 import 'package:news/shared/styles/colors.dart';
 
-class HomeLayout extends StatelessWidget {
+class HomeLayout extends StatefulWidget {
   static const String routeName = "HomeLayout";
 
+  const HomeLayout({super.key});
+
+  @override
+  State<HomeLayout> createState() => _HomeLayoutState();
+}
+
+class _HomeLayoutState extends State<HomeLayout> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,28 +29,37 @@ class HomeLayout extends StatelessWidget {
         centerTitle: true,
         backgroundColor: ColorsData.green,
         title: Text(
-          "News app",
+          category?.name ?? "News App",
           style: GoogleFonts.exo(),
         ),
       ),
       body: FutureBuilder(
-        future: ApiManager.getSources("general"),
+        future: ApiManager.getSources(category?.id ?? ""),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return Center(
+            return const Center(
                 child: CircularProgressIndicator(
               color: Colors.green,
             ));
           }
           if (snapshot.hasError) {
-            return Center(
+            return const Center(
               child: Text("somthing wrong"),
             );
           }
           var source = snapshot.data?.sources ?? [];
-          return TabsScreen(source);
+          return category == null
+              ? CategoryScreen(getSelectedCategory)
+              : TabsScreen(source);
         },
       ),
     );
+  }
+
+  CategoryModel? category;
+
+  void getSelectedCategory(cat) {
+    category = cat;
+    setState(() {});
   }
 }
