eee9c815542de0f69bcca760186a904bbfb83d75
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-28 21:52:49 +0200

    create app bar

--- lib/screens/setting_screen.dart
+++ lib/screens/setting_screen.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../shared/styles/colors.dart';
 
 class SettingScreen extends StatelessWidget {
-  const SettingScreen({super.key});
+  static const String routeName = "SettingScreen";
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(22.r),
+                bottomRight: Radius.circular(22.r))),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: ColorsData.green,
+        title: Text(
+          "Settings",
+          style: GoogleFonts.exo(
+            fontSize: 20.sp,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+    );
   }
 }
