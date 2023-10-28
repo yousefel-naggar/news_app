523cdce59b340ea12f86c4e8088d64156a8fd21b
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-28 19:47:51 +0200

    create new content screen to show the content of screen when click on the new

---
+++ lib/screens/new_content_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewContentScreen extends StatelessWidget {
+  const NewContentScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
