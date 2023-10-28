74818cef29e111cd6b32d1b5ca8dd6fafcbc3a53
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-28 20:29:19 +0200

    when click on the new naviagete drictly to new content screen and send the new to this screen by args and show it

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:news/models/SourcesResponse.dart';
 import 'package:news/screens/%20widget/news_widget.dart';
 import 'package:news/screens/%20widget/tab_widget.dart';
+import 'package:news/screens/new_content_screen.dart';
 import 'package:news/shared/network/remote/api_manager.dart';
 
 class TabsScreen extends StatefulWidget {
@@ -52,9 +53,18 @@ class _TabsScreenState extends State<TabsScreen> {
                 );
               }
               var data = snapshot.data?.articles ?? [];
-              return Expanded(child: ListView.builder(itemBuilder: (context, index) {
-                return NewsWidget(data[index]);
-              },itemCount: data.length,));
+              return Expanded(
+                  child: ListView.builder(
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {
+                      Navigator.pushNamed(context, NewContentScreen.routeName,arguments: data[index]);
+                    },
+                    child: NewsWidget(data[index]),
+                  );
+                },
+                itemCount: data.length,
+              ));
             },
           ),
         ],
