9ee44477c0d7440c0c9e35d01b4c45fbfadca73b
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-28 20:31:11 +0200

    show the data that received by args in this screen

--- lib/screens/new_content_screen.dart
+++ lib/screens/new_content_screen.dart
@@ -1,12 +1,90 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../models/NewsResponse.dart';
+import '../shared/styles/colors.dart';
 
 class NewContentScreen extends StatelessWidget {
-  const NewContentScreen({super.key});
+  static const String routeName = "NewContentScreen";
 
   @override
   Widget build(BuildContext context) {
+    var args = ModalRoute.of(context)!.settings.arguments as Articles;
     return Scaffold(
-
+      appBar: AppBar(
+        shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(22.r),
+                bottomRight: Radius.circular(22.r))),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: ColorsData.green,
+        title: Text(
+          "New Content",
+          style: GoogleFonts.exo(),
+        ),
+      ),
+      body: Padding(
+        padding: REdgeInsets.all(8.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: CachedNetworkImage(
+                imageUrl: args.urlToImage ?? "",
+                height: 200.h,
+                fit: BoxFit.fill,
+                placeholder: (context, url) => Center(
+                    child: CircularProgressIndicator(
+                  color: ColorsData.green,
+                )),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+              ),
+            ),
+            SizedBox(
+              height: 6.h,
+            ),
+            Text("${args.source?.name} *" ?? "",
+                style: TextStyle(color: Colors.grey, fontSize: 13.sp)),
+            SizedBox(
+              height: 6.h,
+            ),
+            Text(
+              args.title ?? "",
+              maxLines: 2,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 18.sp,
+                  overflow: TextOverflow.ellipsis),
+            ),
+            SizedBox(
+              height: 4.h,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text('by " ${args.author} "' ?? "",
+                    style: TextStyle(color: Colors.grey, fontSize: 13.sp),
+                    textAlign: TextAlign.start),
+                Text(args.publishedAt?.substring(0, 10) ?? "",
+                    style: TextStyle(color: Colors.grey, fontSize: 13.sp),
+                    textAlign: TextAlign.end),
+              ],
+            ),
+            SizedBox(
+              height: 30.h,
+            ),
+            Text(
+              args.content ?? "",
+              style: GoogleFonts.poppins(
+                  fontSize: 16.sp, fontWeight: FontWeight.w300),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
