8912021c8fea6983184e1e4ffb71fb6caccbbc86
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-29 16:29:43 +0200

    add Sources type to NewResponse data model

--- lib/models/NewsResponse.dart
+++ lib/models/NewsResponse.dart
@@ -1,8 +1,11 @@
+import 'SourcesResponse.dart';
+
 class NewsResponse {
   NewsResponse({
-      this.status, 
-      this.totalResults, 
-      this.articles,});
+    this.status,
+    this.totalResults,
+    this.articles,
+  });
 
   NewsResponse.fromJson(dynamic json) {
     status = json['status'];
@@ -14,35 +17,28 @@ class NewsResponse {
       });
     }
   }
+
   String? status;
   int? totalResults;
   List<Articles>? articles;
 
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    map['status'] = status;
-    map['totalResults'] = totalResults;
-    if (articles != null) {
-      map['articles'] = articles?.map((v) => v.toJson()).toList();
-    }
-    return map;
-  }
 
 }
 
 class Articles {
   Articles({
-      this.source, 
-      this.author, 
-      this.title, 
-      this.description, 
-      this.url, 
-      this.urlToImage, 
-      this.publishedAt, 
-      this.content,});
+    this.source,
+    this.author,
+    this.title,
+    this.description,
+    this.url,
+    this.urlToImage,
+    this.publishedAt,
+    this.content,
+  });
 
   Articles.fromJson(dynamic json) {
-    source = json['source'] != null ? Source.fromJson(json['source']) : null;
+    source = json['source'] != null ? Sources.fromJson(json['source']) : null;
     author = json['author'];
     title = json['title'];
     description = json['description'];
@@ -51,7 +47,8 @@ class Articles {
     publishedAt = json['publishedAt'];
     content = json['content'];
   }
-  Source? source;
+
+  Sources? source;
   String? author;
   String? title;
   String? description;
@@ -59,33 +56,19 @@ class Articles {
   String? urlToImage;
   String? publishedAt;
   String? content;
-
-  Map<String, dynamic> toJson() {
-    final map = <String, dynamic>{};
-    if (source != null) {
-      map['source'] = source?.toJson();
-    }
-    map['author'] = author;
-    map['title'] = title;
-    map['description'] = description;
-    map['url'] = url;
-    map['urlToImage'] = urlToImage;
-    map['publishedAt'] = publishedAt;
-    map['content'] = content;
-    return map;
-  }
-
 }
 
 class Source {
   Source({
-      this.id, 
-      this.name,});
+    this.id,
+    this.name,
+  });
 
   Source.fromJson(dynamic json) {
     id = json['id'];
     name = json['name'];
   }
+
   String? id;
   String? name;
 
