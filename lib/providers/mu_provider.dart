9119d18927ae8bbd72a1db2b8efa4fdec4a070a8
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-30 23:27:53 +0200

    default of local is english

--- lib/providers/mu_provider.dart
+++ lib/providers/mu_provider.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class MyProvider extends ChangeNotifier {
-  String local = "ar";
+  String local = "en";
 
   changeLanguage(String local) {
     this.local = local;
