ef07abae88d92d3bfd5daa4ef7ef46277022e993
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-30 02:34:30 +0200

    add func getCategoryName that return the name of category

--- lib/layout/layout_screen.dart
+++ lib/layout/layout_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:news/models/category_model.dart';
@@ -35,7 +36,7 @@ class _HomeLayoutState extends State<HomeLayout> {
         centerTitle: true,
         backgroundColor: ColorsData.green,
         title: Text(
-          category?.name ?? "News App",
+          getCategoryName(category),
           style: GoogleFonts.exo(),
         ),
       ),
@@ -78,5 +79,27 @@ class _HomeLayoutState extends State<HomeLayout> {
   void getSelectedCategory(cat) {
     category = cat;
     setState(() {});
+  }
+
+  String getCategoryName(CategoryModel? categoryModel) {
+    if (categoryModel?.name == "Sports") {
+      return AppLocalizations.of(context)!.sports;
+    }
+    if (categoryModel?.name == "Politics") {
+      return AppLocalizations.of(context)!.politics;
+    }
+    if (categoryModel?.name == "Business") {
+      return AppLocalizations.of(context)!.business;
+    }
+    if (categoryModel?.name == "Health") {
+      return AppLocalizations.of(context)!.health;
+    }
+    if (categoryModel?.name == "Entertainment") {
+      return AppLocalizations.of(context)!.entertainment;
+    }
+    if (categoryModel?.name == "Science") {
+      return AppLocalizations.of(context)!.science;
+    }
+    return AppLocalizations.of(context)!.appName;
   }
 }
--- lib/screens/ widget/category_item_widget.dart
+++ lib/screens/ widget/category_item_widget.dart
@@ -1,37 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 import '../../models/category_model.dart';
 
-class CategoryItemWidget extends StatelessWidget {
+class CategoryItemWidget extends StatefulWidget {
   CategoryModel categoryModel;
 
   CategoryItemWidget(this.categoryModel, {super.key});
 
   @override
+  State<CategoryItemWidget> createState() => _CategoryItemWidgetState();
+}
+
+class _CategoryItemWidgetState extends State<CategoryItemWidget> {
+  @override
   Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
-          color: categoryModel.color,
+          color: widget.categoryModel.color,
           borderRadius: BorderRadius.circular(12.r)),
       margin: REdgeInsets.symmetric(horizontal: 10),
       height: 140.h,
       child: Column(children: [
         SizedBox(height: 10.h),
         Image.asset(
-          categoryModel.img,
+          widget.categoryModel.img,
           height: 100.h,
           fit: BoxFit.fill,
         ),
         Spacer(),
         Text(
-          categoryModel.name,
-          style: TextStyle(fontSize: 17.5.sp, color: Colors.white),
+          getCategoryName(widget.categoryModel),
+          style: TextStyle(fontSize: 18.5.sp, color: Colors.white),
         ),
         SizedBox(
           height: 7.h,
         )
       ]),
     );
+  }
+
+  String getCategoryName(CategoryModel categoryModel) {
+    if (categoryModel.name == "Sports") {
+      return AppLocalizations.of(context)!.sports;
+    }
+    if (categoryModel.name == "Politics") {
+      return AppLocalizations.of(context)!.politics;
+    }
+    if (categoryModel.name == "Business") {
+      return AppLocalizations.of(context)!.business;
+    }
+    if (categoryModel.name == "Health") {
+      return AppLocalizations.of(context)!.health;
+    }
+    if (categoryModel.name == "Entertainment") {
+      return AppLocalizations.of(context)!.entertainment;
+    }
+    if (categoryModel.name == "Science") {
+      return AppLocalizations.of(context)!.science;
+    }
+    return "";
   }
 }
