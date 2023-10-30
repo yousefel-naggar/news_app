43fb435dbaf5389df20087c56cd2775cf5685f50
yousefel-naggar <yousef.a.naggar@gmail.com>
2023-10-30 23:14:49 +0200

    add provider to control in language of app

--- lib/bottom_sheets/lanuage_bottom_sheet.dart
+++ lib/bottom_sheets/lanuage_bottom_sheet.dart
@@ -1,50 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:news/shared/styles/colors.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/mu_provider.dart';
 
 class LanguageBottomSheet extends StatelessWidget {
   const LanguageBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var pro = Provider.of<MyProvider>(context);
     return Container(
       padding: REdgeInsets.all(16),
       height: 200.h,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "English",
-                style: GoogleFonts.exo(
-                    fontSize: 21.sp, fontWeight: FontWeight.w200),
-              ),
-              Visibility(
-                visible: true,
-                child: Icon(
-                  Icons.check,
+          InkWell(
+            onTap: () {
+              pro.changeLanguage("en");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "English",
+                  style: GoogleFonts.exo(
+                      fontSize: 21.sp,
+                      color:
+                          pro.local == "en" ? ColorsData.green : Colors.black,
+                      fontWeight: FontWeight.w200),
                 ),
-              ),
-            ],
+                Visibility(
+                  visible: pro.local == "en" ? true : false,
+                  child: Icon(
+                    color: ColorsData.green,
+                    Icons.check,
+                  ),
+                ),
+              ],
+            ),
           ),
           SizedBox(height: 10.h),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Text(
-                "Arabic",
-                style: GoogleFonts.exo(
-                    fontSize: 21.sp, fontWeight: FontWeight.w200),
-              ),
-              Visibility(
-                visible: true,
-                child: Icon(
-                  Icons.check,
+          InkWell(
+            onTap: () {
+              pro.changeLanguage("ar");
+            },
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  pro.local == "ar" ? "عر بي" : "Arabic",
+                  style: GoogleFonts.exo(
+                      fontSize: 21.sp,
+                      color:
+                          pro.local == "ar" ? ColorsData.green : Colors.black,
+                      fontWeight: FontWeight.w200),
                 ),
-              ),
-            ],
+                Visibility(
+                  visible: pro.local == "ar" ? true : false,
+                  child: Icon(
+                    color: ColorsData.green,
+                    Icons.check,
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
